7087cf0e2f0c952aed622bb645b3b514a2f82863
Ion Talmaci <ntalmaci171@gmail.com>
2018-10-02 17:45:49 +0300

    my first commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+(function($){
+  $(document).ready(function () {
+
+    $(document).foundation();
+    
+  });
+})(jQuery);
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYXBwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiKGZ1bmN0aW9uKCQpe1xyXG4gICQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uICgpIHtcclxuXHJcbiAgICAkKGRvY3VtZW50KS5mb3VuZGF0aW9uKCk7XHJcbiAgICBcclxuICB9KTtcclxufSkoalF1ZXJ5KTsiXX0=
